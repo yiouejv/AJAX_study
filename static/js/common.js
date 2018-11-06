05afbb88b3fa4e86989b835a25bd9d28ee8babfd
yiouejv <yiouejv@126.com>
2018-11-06 11:07:10 +0800

    chagen1

---
+++ static/js/common.js
@@ -0,0 +1,13 @@
+$(function () {
+    var cxhr_btn = $('#cxhr_btn');
+    cxhr_btn.click(function () {
+        // 判断浏览器是否支持XMLHttpRequest
+        if (window.XMLHttpRequest){
+            console.log(new XMLHttpRequest());
+            return new XMLHttpRequest();
+        }else{
+            console.log(new ActiveXObject());
+            return new ActiveXObject('Microsoft.XMLHTTP');
+        }
+    });
+});
